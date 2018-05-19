e2c08b0b53ce126a01d471997fd4542dee829b45
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/OsmGeocodeSample/MainForm.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraMap
+
+Namespace OsmGeocodeSample
+    Partial Public Class MainForm
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        #Region "#ConfigureMapLayers"
+        Private Sub OnFormLoad(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            mapControl.Layers.AddRange(New LayerBase() { _
+                New ImageLayer With { _
+                    .DataProvider = New OpenStreetMapDataProvider() With {.TileUriTemplate = "YOUR_TILE_SERVICE.com/{1}/{2}/{3}"} _
+                }, _
+                _
+                New InformationLayer With {.DataProvider = New OsmGeocodeDataProvider()} _
+            })
+        End Sub
+        #End Region ' #ConfigureMapLayers
+    End Class
+End Namespace
